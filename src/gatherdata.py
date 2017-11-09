bbe57e387c303205347b609bb0ce083463e8e62c
skitti <33321808+skitti@users.noreply.github.com>
2017-11-09 16:57:23 +0100

    Add files via upload

---
+++ src/gatherdata.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import rospy
+
+from modeltruck_platooning.msg import VehicleState
+
+xlist=[]
+ylist=[]
+anglelist=[]
+
+def truckstate():
+	rospy.init_node('truckstate', anonymous=True)
+	rospy.Subscriber("Mocapstate1",VehicleState,callback)
+	rospy.spin()
+
+def callback(data):
+	x = data.x
+	y = data.y
+	angle = data.yaw
+
+	xlist.append(x)
+	ylist.append(y)
+	anglelist.append(angle)
+	
+	print('x_coordinate:', x)
+	print('y_coordinate:', y)
+	print('Angle :', angle)
+	#fixa service där du specifierar ett tidsintervall mellan datainhämtningarna
+	#om inga nya värden registreras efter ett antal "samplingar", skickas datalistorna
+	#till log() där värdena loggas i en txtfil
+        if xlist[end-5]==xlist[end]:
+                log(xlist,ylist,anglelist)
+        else:
+                continue
+	
+def log(xlist,ylist,anglelist):
+        fil=open("mologg.txt","w") #se till att en txtfil med rätt namn finns i samma mapp
+	for i in xlist:
+                fil.write(str(xlist[i-1])+" "+str(ylist[i-1])+" "+str(anglelist[i-1])+'\n')
+        fil.close()        
+        
+if __name__ == '__main__':
+    truckstate()
+
