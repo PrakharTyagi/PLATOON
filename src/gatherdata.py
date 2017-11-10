314e11c5ec64d8c6f6b2ba2db7abd85388a132ce
skitti <masso.gabriel@gmail.com>
2017-11-10 15:43:00 +0100

    testa gitta

--- src/gatherdata.py
+++ src/gatherdata.py
@@ -24,20 +24,25 @@ def callback(data):
 	print('x_coordinate:', x)
 	print('y_coordinate:', y)
 	print('Angle :', angle)
+	
 	#fixa service där du specifierar ett tidsintervall mellan datainhämtningarna
 	#om inga nya värden registreras efter ett antal "samplingar", skickas datalistorna
 	#till log() där värdena loggas i en txtfil
-        if xlist[end-5]==xlist[end]:
-                log(xlist,ylist,anglelist)
-        else:
-                continue
+	
+	if len(xlist) < 6:
+		pass
+	elif xlist[end-5]==xlist[end]:
+		log(xlist,ylist,anglelist)            
+	else:
+		pass
 	
 def log(xlist,ylist,anglelist):
-        fil=open("mologg.txt","w") #se till att en txtfil med rätt namn finns i samma mapp
+	fil=open("mologg.txt","w") #se till att en txtfil med rätt namn finns i samma mapp
 	for i in xlist:
-                fil.write(str(xlist[i-1])+" "+str(ylist[i-1])+" "+str(anglelist[i-1])+'\n')
-        fil.close()        
-        
+		fil.write(str(xlist[i-1])+" "+str(ylist[i-1])+" "+str(anglelist[i-1])+'\n')
+	fil.close()        
+
 if __name__ == '__main__':
     truckstate()
+
 
